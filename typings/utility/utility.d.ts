e0b50101a908bae2e21de07a6331f6d2fe01879f
王一卜 <wangyibu@gridsum.com>
2016-07-13 13:49:51 +0800

    暂时定义

--- typings/utility/utility.d.ts
+++ typings/utility/utility.d.ts
@@ -1,20 +1,16 @@
+declare var utility:basic.api;
+
 declare module "utility" {
-    export = ut;
+     interface LoDashStatic {
+        md5(value?: any);
+    }
+    export = utility;
 }
 
-declare var ut: ut.LoDashStatic;
-
-declare module ut {
-     interface LoDashStatic {
-        /**
-         * Checks if value is classified as a Date object.
-         * @param value The value to check.
-         *
-         * @return Returns true if value is correctly classified, else false.
-         */
-        md5(value?: any): value is Date;
+declare module basic {
+     interface api {
+        md5(value?: any);
     }
-
 }
 
 
