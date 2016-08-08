7866a867920c8575da63920721e6ac38cc1727c0
王一卜 <wangyibu@gridsum.com>
2016-08-08 20:12:19 +0800

    1

---
+++ Design_Pattern/decorator2.ts
@@ -0,0 +1,31 @@
+module PATTERN {
+
+  export interface Precure {
+    getTitle(): string;
+  }
+
+  export class SweetPrecure implements Precure {
+    getTitle(): string {
+      return 'title1';
+    }
+  }
+
+  export class SmilePrecure implements Precure {
+    getTitle(): string {
+      return 'title2';
+    }
+  }
+
+  export class MovieDecorator implements Precure {
+    private subTitle: string = 'title3';
+
+    constructor(private precure: Precure) {}
+
+    getTitle(): string {
+      return this.subTitle + this.precure.getTitle();
+    }
+  }
+  
+  var sweetPrecureMovie = new MovieDecorator(new SweetPrecure());
+  var smilePrecureMovie = new MovieDecorator(new SmilePrecure());
+}
