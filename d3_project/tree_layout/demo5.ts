ef815bf72f2d2dac8537caa9b4eadc1a68993448
王一卜 <wangyibu@gridsum.com>
2016-08-10 16:58:14 +0800

    暂时迁入

---
+++ d3_project/tree_layout/demo5.ts
@@ -0,0 +1,51 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+
+
+var svg = d3.select("svg");
+
+var width = +svg.attr("width");
+var height = +svg.attr("height");
+var g = svg.append("g").attr("transform", "translate(40,0)");
+
+// var cluster = d3.cluster()
+//     .size([height, width - 160]);
+var cluster = d3.layout.cluster().size([height, width - 160]);
+
+var stratify = d3.stratify()
+    .parentId(function (d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
+
+d3.csv("demo5.csv", function (error, data) {
+    if (error) throw error;
+
+    var root = stratify(data)
+        .sort(function (a, b) { return (a.height - b.height) || a.id.localeCompare(b.id); });
+
+    cluster(root);
+
+    var link = g.selectAll(".link")
+        .data(root.descendants().slice(1))
+        .enter().append("path")
+        .attr("class", "link")
+        .attr("d", function (d: any) {
+            return "M" + d.y + "," + d.x
+                + "C" + (d.parent.y + 100) + "," + d.x
+                + " " + (d.parent.y + 100) + "," + d.parent.x
+                + " " + d.parent.y + "," + d.parent.x;
+        });
+
+    var node = g.selectAll(".node")
+        .data(root.descendants())
+        .enter().append("g")
+        .attr("class", function (d: any) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+        .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; })
+
+    node.append("circle")
+        .attr("r", 2.5);
+
+    node.append("text")
+        .attr("dy", 3)
+        .attr("x", function (d: any) { return d.children ? -8 : 8; })
+        .style("text-anchor", function (d: any) { return d.children ? "end" : "start"; })
+        .text(function (d: any) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
+});
