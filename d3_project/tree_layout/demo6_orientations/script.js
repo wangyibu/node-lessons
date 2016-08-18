9574ee22fd6232017142ebf92b7432c62ecbce9e
王一卜 <wangyibu@gridsum.com>
2016-08-18 15:15:43 +0800

    实现反向树结构

--- d3_project/tree_layout/demo6_orientations/script.js
+++ d3_project/tree_layout/demo6_orientations/script.js
@@ -1,18 +1,18 @@
 var test;
 (function (test) {
     var orientation;
-    (function (orientation_1) {
-        var margin = { top: 10, right: 10, bottom: 10, left: 10 }, width = 400 - margin.left - margin.right, height = 500 - margin.top - margin.bottom;
+    (function (orientation) {
+        var margin = { top: 10, right: 10, bottom: 10, left: 10 }, width = 1200 - margin.left - margin.right, height = 700 - margin.top - margin.bottom;
         var orientations = {
-            // "right-to-left": {
-            //     size: [height, width],
-            //     x: function (d) {
-            //         return width - d.y;
-            //     },
-            //     y: function (d) {
-            //         return d.x;
-            //     }
-            // },
+            "right-to-left": {
+                size: [height, width],
+                x: function (d) {
+                    return width - d.y;
+                },
+                y: function (d) {
+                    return d.x;
+                }
+            },
             "left-to-right": {
                 size: [height, width],
                 x: function (d) {
@@ -23,17 +23,26 @@ var test;
                 }
             },
         };
-        var svg = d3.select("body").selectAll("svg")
-            .data(d3.entries(orientations)) // 转化数组 【 key ： left-to-right   value ：{size:[heigh,width],x:func,y:func}
-            .enter().append("svg")
+        var svg = d3.select("body")
+            .append('svg')
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .style({
             'border': '1px solid red',
             'margin-right': '10px'
-        })
-            .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        });
+        // var svg = d3.select("body").selectAll("svg")
+        //     .data(d3.entries(orientations)) // 转化数组 【 key ： left-to-right   value ：{size:[heigh,width],x:func,y:func}
+        //     .enter().append("svg")
+        //     .attr("width", width + margin.left + margin.right)
+        //     .attr("height", height + margin.top + margin.bottom)
+        //     .style({
+        //         'border': '1px solid red',
+        //         'margin-right':'10px'
+        //     })
+        //     .append("g")
+        //     // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        //     .attr("transform", "translate(" + width/2 + "," + height / 2 + ")");
         // svg.append('a')
         //   .attr("xlink:href", "http://www.baidu.com")
         //   .append("rect")
@@ -51,34 +60,88 @@ var test;
         d3.json("doc.json", function (error, root) {
             if (error)
                 throw error;
-            svg.each(function (orientation) {
-                var svg = d3.select(this), o = orientation.value; // orientation.value  = {size: [height, width], x: function (d) { return d.y; }, y: function (d) { return d.x; }}
-                var son = d3.entries(root.children);
-                var leftTree = {
-                    children: []
-                };
-                var rightTree = {
-                    children: []
-                };
-                son.forEach(function (d, index, arr) {
-                    if (d.value.orientation == 'left') {
-                        leftTree.children.push(d.value);
+            // 计算2个树
+            var son = d3.entries(root.children);
+            var leftTree = {
+                children: [],
+                option: {
+                    size: [height, width],
+                    x: function (d) {
+                        return d.y;
+                    },
+                    y: function (d) {
+                        return d.x;
                     }
-                    else {
-                        rightTree.children.push(d.value);
+                }
+            };
+            var rightTree = {
+                children: [],
+                option: {
+                    size: [height, width],
+                    x: function (d) {
+                        return width - d.y;
+                    },
+                    y: function (d) {
+                        return d.x;
                     }
-                });
-                console.log(leftTree, rightTree);
+                }
+            };
+            son.forEach(function (d, index, arr) {
+                if (d.value.orientation == 'left') {
+                    leftTree.children.push(d.value);
+                }
+                else {
+                    rightTree.children.push(d.value);
+                }
+            });
+            var tree = {
+                left: leftTree,
+                right: rightTree
+            };
+            // console.log(leftTree, rightTree);
+            var gAll = svg.selectAll('g')
+                .data(d3.entries(tree))
+                .enter().append('g')
+                .attr("transform", function (d) {
+                var center = {
+                    x: width / 2,
+                    y: height / 2
+                };
+                if (d.key == 'right') {
+                    center.x = -center.x;
+                }
+                return "translate(" + center.x + "," + center.y + ")";
+            });
+            // var svg = d3.select("body").selectAll("svg")
+            //     .data(d3.entries<IChild>(tree)) // 转化数组 【 key ： left-to-right   value ：{size:[heigh,width],x:func,y:func}
+            //     .enter().append("svg")
+            //     .attr("width", width + margin.left + margin.right)
+            //     .attr("height", height + margin.top + margin.bottom)
+            //     .style({
+            //         'border': '1px solid red',
+            //         'margin-right': '10px'
+            //     })
+            //     .append("g")
+            //     // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            //     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+            gAll.each(function (allTree) {
+                // if (allTree.key == 'left') {
+                //     console.log(allTree.value);
+                // } else {
+                //     console.log(allTree.value);
+                // }
+                var node = allTree.value;
+                var group = d3.select(this), o = node.option; // orientation.value  = {size: [height, width], x: function (d) { return d.y; }, y: function (d) { return d.x; }}
                 // Compute the layout.
                 //   var tree = d3.layout.tree().size(o.size),
                 var tree = d3.layout.tree().nodeSize([30, 200]), // nodeSize  [height,width] height 两个点之间的垂直距离  width ?? 未知
-                nodes = tree.nodes(root), links = tree.links(nodes);
+                nodes = tree.nodes(node), links = tree.links(nodes);
                 var tree_left = d3.layout.tree().nodeSize([30, 200]);
                 var nodes_left = nodes.forEach(function (d) {
                     d.y = d.depth * 80; //  控制每一级别的宽度
                 });
                 // Create the link lines.
-                svg.selectAll(".link")
+                group.selectAll(".link")
                     .data(links)
                     .enter().append("path")
                     .attr("class", "link")
@@ -86,7 +149,7 @@ var test;
                     return [o.x(d), o.y(d)];
                 }));
                 // Create the node circles.
-                svg.selectAll(".node")
+                group.selectAll(".node")
                     .data(nodes)
                     .enter().append("circle")
                     .attr("class", "node")
--- d3_project/tree_layout/demo6_orientations/script.ts
+++ d3_project/tree_layout/demo6_orientations/script.ts
@@ -1,25 +1,32 @@
 module test.orientation {
 
-    interface IChild{
-        children:IChild[];
-        orientation?:string;
+    interface IChild {
+        children: IChild[];
+        orientation?: string;
+        option?: IOrientationOption;
+    }
+
+    interface IOrientationOption {
+        size: Array<number>;
+        x(d: any): number;
+        y(d: any): number;
     }
 
     var margin = { top: 10, right: 10, bottom: 10, left: 10 },
-        width = 400 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        width = 1200 - margin.left - margin.right,
+        height = 700 - margin.top - margin.bottom;
 
     var orientations = {
 
-        // "right-to-left": {
-        //     size: [height, width],
-        //     x: function (d) {
-        //         return width - d.y;
-        //     },
-        //     y: function (d) {
-        //         return d.x;
-        //     }
-        // },
+        "right-to-left": {
+            size: [height, width],
+            x: function (d) {
+                return width - d.y;
+            },
+            y: function (d) {
+                return d.x;
+            }
+        },
         "left-to-right": {
             size: [height, width],
             x: function (d) {
@@ -42,18 +49,27 @@ module test.orientation {
         // }
     };
 
-    var svg = d3.select("body").selectAll("svg")
-        .data(d3.entries(orientations)) // 转化数组 【 key ： left-to-right   value ：{size:[heigh,width],x:func,y:func}
-        .enter().append("svg")
+    var svg = d3.select("body")
+        .append('svg')
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .style({
             'border': '1px solid red',
-            'margin-right':'10px'
-        })
-        .append("g")
-        // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        .attr("transform", "translate(" + width/2 + "," + height / 2 + ")");
+            'margin-right': '10px'
+        });
+
+    // var svg = d3.select("body").selectAll("svg")
+    //     .data(d3.entries(orientations)) // 转化数组 【 key ： left-to-right   value ：{size:[heigh,width],x:func,y:func}
+    //     .enter().append("svg")
+    //     .attr("width", width + margin.left + margin.right)
+    //     .attr("height", height + margin.top + margin.bottom)
+    //     .style({
+    //         'border': '1px solid red',
+    //         'margin-right':'10px'
+    //     })
+    //     .append("g")
+    //     // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    //     .attr("transform", "translate(" + width/2 + "," + height / 2 + ")");
 
     // svg.append('a')
     //   .attr("xlink:href", "http://www.baidu.com")
@@ -75,42 +91,101 @@ module test.orientation {
     d3.json("doc.json", function (error, root) {
         if (error) throw error;
 
-        svg.each(function (orientation) {           // each(func: (datum: Datum, index: number, outerIndex: number) => any): Selection<Datum>;
-            var svg = d3.select(this),
-                o = orientation.value;                // orientation.value  = {size: [height, width], x: function (d) { return d.y; }, y: function (d) { return d.x; }}
-                    
-            var son = d3.entries<IChild>(root.children);
-            var leftTree = <IChild>{
-                children:[]
-            }
-            var rightTree = <IChild>{
-                children:[]
-            }
-            son.forEach((d,index,arr)=>{
-                if(d.value.orientation == 'left'){
-                    leftTree.children.push(d.value);
-                }else{
-                   rightTree.children.push(d.value);
+        // 计算2个树
+        var son = d3.entries<IChild>(root.children);
+        var leftTree = <IChild>{
+            children: [],
+            option: {
+                size: [height, width],
+                x: function (d) {
+                    return d.y;
+                },
+                y: function (d) {
+                    return d.x;
                 }
+            }
+        }
+        var rightTree = <IChild>{
+            children: [],
+            option: {
+                size: [height, width],
+                x: function (d) {
+                    return width - d.y;
+                },
+                y: function (d) {
+                    return d.x;
+                }
+            }
+        }
+        son.forEach((d, index, arr) => {
+            if (d.value.orientation == 'left') {
+                leftTree.children.push(d.value);
+            } else {
+                rightTree.children.push(d.value);
+            }
+        });
+
+        var tree = {
+            left: leftTree,
+            right: rightTree
+        }
+
+        // console.log(leftTree, rightTree);
+        var gAll = svg.selectAll('g')
+            .data(d3.entries<IChild>(tree))
+            .enter().append('g')
+            .attr("transform", (d) => {
+                var center = {
+                    x : width/2,
+                    y : height/2
+                }
+                if(d.key == 'right'){
+                    center.x = -center.x;
+                }
+                return "translate(" + center.x + "," + center.y + ")"
             });
 
-            
-            console.log(leftTree,rightTree);
+
+
+
+        // var svg = d3.select("body").selectAll("svg")
+        //     .data(d3.entries<IChild>(tree)) // 转化数组 【 key ： left-to-right   value ：{size:[heigh,width],x:func,y:func}
+        //     .enter().append("svg")
+        //     .attr("width", width + margin.left + margin.right)
+        //     .attr("height", height + margin.top + margin.bottom)
+        //     .style({
+        //         'border': '1px solid red',
+        //         'margin-right': '10px'
+        //     })
+        //     .append("g")
+        //     // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        //     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        gAll.each(function(allTree){           // each(func: (datum: Datum, index: number, outerIndex: number) => any): Selection<Datum>;
+
+            // if (allTree.key == 'left') {
+            //     console.log(allTree.value);
+            // } else {
+            //     console.log(allTree.value);
+            // }
+            var node = <IChild>allTree.value;
+
+            var group = d3.select(this),
+                o = node.option;                // orientation.value  = {size: [height, width], x: function (d) { return d.y; }, y: function (d) { return d.x; }}
+
+
             // Compute the layout.
             //   var tree = d3.layout.tree().size(o.size),
             var tree = d3.layout.tree().nodeSize([30, 200]),// nodeSize  [height,width] height 两个点之间的垂直距离  width ?? 未知
-                nodes = tree.nodes(root),
+                nodes = tree.nodes(node),
                 links = tree.links(nodes);
-            var tree_left = d3.layout.tree().nodeSize([30,200]);
-            var nodes_left = 
-
-
-            nodes.forEach((d) => {
-                d.y = d.depth * 80; //  控制每一级别的宽度
-            })
+            var tree_left = d3.layout.tree().nodeSize([30, 200]);
+            var nodes_left = nodes.forEach((d) => {
+                    d.y = d.depth * 80; //  控制每一级别的宽度
+                })
 
             // Create the link lines.
-            svg.selectAll(".link")
+            group.selectAll(".link")
                 .data(links)
                 .enter().append("path")
                 .attr("class", "link")
@@ -121,7 +196,7 @@ module test.orientation {
                 );
 
             // Create the node circles.
-            svg.selectAll(".node")
+            group.selectAll(".node")
                 .data(nodes)
                 .enter().append("circle")
                 .attr("class", "node")
