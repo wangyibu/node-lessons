d564ecf702d7709aa4a65c381164b0f3d8c8de2a
王一卜 <wangyibu@gridsum.com>
2016-08-18 16:29:32 +0800

    临时迁入

---
+++ d3_project/tree_layout/demo1/script.js
@@ -0,0 +1,67 @@
+var test;
+(function (test) {
+    var demo1;
+    (function (demo1) {
+        var margin = { top: 10, right: 10, bottom: 10, left: 10 }, width = 1200 - margin.right - margin.left, height = 700 - margin.top - margin.bottom;
+        var i = 0;
+        var tree = d3.layout.tree()
+            .size([height, width])
+            .separation(function (a, b) {
+            return 400;
+        });
+        var diagonal = d3.svg.diagonal()
+            .projection(function (d) { return [d.y, d.x]; });
+        var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.right + margin.left)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + 100 + "," + 100 + ")");
+        var root;
+        d3.json('doc.json', function (err, data) {
+            root = data;
+            update(root);
+        });
+        // root = treeData[0];
+        // update(root);
+        function update(source) {
+            // Compute the new tree layout.
+            var nodes = tree.nodes(root).reverse(), links = tree.links(nodes);
+            // Normalize for fixed-depth.
+            nodes.forEach(function (d) {
+                d.y = d.depth * 180;
+            });
+            // Declare the nodes…
+            var node = svg.selectAll("g.node")
+                .data(nodes, function (d) { return d.id || (d.id = ++i); });
+            // Enter the nodes.
+            var nodeEnter = node.enter().append("g")
+                .attr("class", "node")
+                .attr("transform", function (d) {
+                return "translate(" + d.y + "," + d.x + ")";
+            });
+            nodeEnter.append("circle")
+                .attr("r", function (d) { return d.value; })
+                .style("stroke", function (d) { return d.type; })
+                .style("fill", function (d) { return d.level; });
+            nodeEnter.append("text")
+                .attr("x", function (d) {
+                return d.children || d._children ?
+                    (d.value + 4) * -1 : d.value + 4;
+            })
+                .attr("dy", ".35em")
+                .attr("text-anchor", function (d) {
+                return d.children || d._children ? "end" : "start";
+            })
+                .text(function (d) { return d.name; })
+                .style("fill-opacity", 1);
+            // Declare the links…
+            var link = svg.selectAll("path.link")
+                .data(links, function (d) { return d.target.id; });
+            // Enter the links.
+            link.enter().insert("path", "g")
+                .attr("class", "link")
+                .style("stroke", function (d) { return d.target.level; })
+                .attr("d", diagonal);
+        }
+    })(demo1 = test.demo1 || (test.demo1 = {}));
+})(test || (test = {}));
