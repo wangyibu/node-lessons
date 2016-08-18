f6c6cdf859377056e3d15061c764a0bfb91a5f54
王一卜 <wangyibu@gridsum.com>
2016-08-18 16:39:07 +0800

    暂时优化

--- d3_project/tree_layout/demo2/script.ts
+++ d3_project/tree_layout/demo2/script.ts
@@ -15,293 +15,298 @@
 //     children: point[];
 // }
 
-var margin = { top: 20, right: 40, bottom: 20, left: 40 },
-    rectW = 200,
-    rectH = 60,
-    width = 1200,
-    height = 700;
+module test.demo2 {
 
 
-var i = 0,
-    duration = 750,
-    root;
-var zm;
 
-//Redraw for zoom
-var redraws = () => {
-    //console.log("here", d3.event.translate, d3.event.scale);
-    svg.attr("transform",
-        "translate(" + (<d3.ZoomEvent>d3.event).translate + ")"
-        + " scale(" + (<d3.ZoomEvent>d3.event).scale + ")");
-    // 滚动改变字体大小
-    svg.selectAll("text")
-        .style("font-size", (d) => {
-            return 10 / (<d3.ZoomEvent>d3.event).scale < 6 ? 6 : 10 / (<d3.ZoomEvent>d3.event).scale + "px";
+    var margin = { top: 20, right: 40, bottom: 20, left: 40 },
+        rectW = 200,
+        rectH = 60,
+        width = 1200,
+        height = 700;
+
+
+    var i = 0,
+        duration = 750,
+        root;
+    var zm;
+
+    //Redraw for zoom
+    var redraws = () => {
+        //console.log("here", d3.event.translate, d3.event.scale);
+        svg.attr("transform",
+            "translate(" + (<d3.ZoomEvent>d3.event).translate + ")"
+            + " scale(" + (<d3.ZoomEvent>d3.event).scale + ")");
+        // 滚动改变字体大小
+        svg.selectAll("text")
+            .style("font-size", (d) => {
+                return 10 / (<d3.ZoomEvent>d3.event).scale < 6 ? 6 : 10 / (<d3.ZoomEvent>d3.event).scale + "px";
+            });
+    }
+
+    zm = d3.behavior.zoom().scaleExtent([0.5, 10]).on("zoom", redraws);
+    zm.translate([margin.left, (height - rectH) / 2]);
+
+    // var tree = d3.layout.tree().size([height/2, width/2]);
+    var tree = d3.layout.tree().nodeSize([100, 200]); //nodeSize  [height,width] height 两个点之间的垂直距离  width ?? 未知
+
+    var diagonal = d3.svg.diagonal()
+        .projection((d) => {
+            return [d.y + rectW, d.x + rectH / 2];  // 二次调整 参数
         });
-}
 
-zm = d3.behavior.zoom().scaleExtent([0.5, 10]).on("zoom", redraws);
-zm.translate([margin.left, (height - rectH) / 2]);
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .call(zm)
+        .append("g")
+        .attr("transform", (d) => {
+            return "translate(" + margin.left + "," + (height - rectH) / 2 + ")";
+        });
 
-// var tree = d3.layout.tree().size([height/2, width/2]);
-var tree = d3.layout.tree().nodeSize([70, 200]); //nodeSize  [height,width] height 两个点之间的垂直距离  width ?? 未知
+    var clip = svg.append("svg:clipPath")
+        .attr("id", "clip")
+        .append("svg:rect")
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('width', 200)
+        .attr("height", 60)
+        .attr("id", "clip1");
 
-var diagonal = d3.svg.diagonal()
-    .projection((d) => {
-        return [d.y + rectW, d.x + rectH / 2];  // 二次调整 参数
+
+    d3.json("doc.json", (error, data) => {
+        if (error) throw error;
+        root = data;
+        root.x0 = 0;   // 最开始的起点展开前x0坐标
+        root.y0 = 0;   // 最开始的起点展开前y0坐标
+
+
+        // 转化  root 节点 children 字节点 _children
+        var collapse = (d) => {
+            if (d.children) {
+                d._children = d.children;
+                d._children.forEach(collapse);
+                d.children = null;
+            }
+        }
+
+        data.children.forEach(collapse);
+        update(root);
     });
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .call(zm)
-    .append("g")
-    .attr("transform", (d) => {
-        return "translate(" + margin.left + "," + (height - rectH) / 2 + ")";
-    });
+    function wrap(text, width) {
+        text.each(function () {
+            var text = d3.select(this),
+                words = text.text().split(/\s+/).reverse(),
+                word,
+                line = [],
+                lineNumber = 0,
+                lineHeight = 1.1, // ems
+                y = text.attr("y"),
+                dy = parseFloat(text.attr("dy")),
+                tspan = text.text(null).append("tspan").attr("y", y).attr("dy", dy + "em");
+            while (word = words.pop()) {
+                line.push(word);
+                tspan.text(line.join(" "));
+                if ((<SVGTextContentElement>tspan.node()).getComputedTextLength() > width) {
+                    line.pop();
+                    tspan.text(line.join(" "));
+                    line = [word];
+                    tspan = text.append("tspan").attr("x", (d) => {
+                        return d.textPadding;
+                    }).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+                }
+            }
+        });
+    }
 
-var clip = svg.append("svg:clipPath")
-    .attr("id", "clip")
-    .append("svg:rect")
-    .attr('x', 0)
-    .attr('y', 0)
-    .attr('width', 200)
-    .attr("height", 60)
-    .attr("id", "clip1");
+    var update = (source) => {
 
 
-d3.json("doc.json", (error, data) => {
-    if (error) throw error;
-    root = data;
-    root.x0 = 0;   // 最开始的起点展开前x0坐标
-    root.y0 = 0;   // 最开始的起点展开前y0坐标
+        // Compute the new tree layout. 计算父布局并返回一组节点 / 计算树节点的父-子连接。
+        var nodes = tree.nodes(root).reverse(),
+            links = tree.links(nodes);
 
+        // Normalize for fixed-depth.  改变各个层级的距离
+        nodes.forEach((d) => {
+            //  d.y = d.depth * 180;
+            d.y = d.depth * 340;     // translate(180)  0 180 360
+        });
 
-    // 转化  root 节点 children 字节点 _children
-    var collapse = (d) => {
+        // Update the nodes…
+        var node = svg.selectAll("g.node")
+            .data<any>(nodes, (d) => {
+                return d.id || (d.id = ++i);
+            });
+
+        // Enter any new nodes at the parent's previous position.  操作之后移动横纵坐标的位置 绑定click事件
+        var nodeEnter = node.enter().append("g")
+            .attr("class", "node")
+            .attr("transform", (d) => {
+                return "translate(" + source.y0 + "," + source.x0 + ")";
+            });
+
+        nodeEnter.append('rect')
+            .attr("width", rectW)
+            .attr("height", rectH)
+            .attr("stroke", "#2ab3ed")
+            .attr("stroke-width", 1)
+            .attr("x", (d) => {
+                return d.parent ? 200 - rectW / 2 : rectW / 2;
+            })
+            .style("fill", "#e8eef7")
+            .attr("clip-path", function (d, i) { return "url(#clip1)"; });
+
+        nodeEnter.append('rect')
+            .attr("width", rectW - 1)
+            .attr("height", 20)
+            .attr("y", (d) => {
+                return 39.5;
+            })
+            .attr("x", (d) => {
+                return d.parent ? 200.5 - rectW / 2 : 0.5 + rectW / 2;
+            })
+            .style("fill", "#c8dbf7");
+
+        nodeEnter.append("text")
+            .attr("x", (d) => {
+                return d.parent ? 210 - rectW / 2 : 10 + rectW / 2;
+            })
+            .attr("y", (d) => {
+                return rectH - 5;
+            })
+            .attr("text-anchor", (d) => {
+                // return d.children || d._children ? "end" : "start";
+                return "start";
+            })
+            .text((d) => {
+                return "页面访问量:34333112";
+            })
+            .style("fill-opacity", 1e-6);
+
+        //添加节点 如果有字节点颜色加深
+        nodeEnter.append("circle")
+            .attr("cx", (datum, index, outerIndex) => {
+                return rectW + rectW / 2;
+            })
+            .attr("cy", (datum, index, outerIndex) => {
+                return rectH / 2;
+            })
+            .attr("stroke", "black")
+            .attr("stroke-width", 1)
+            .attr("r", 6)
+            .style("fill", function (d) {
+                return d._children ? "lightsteelblue" : "#fff";
+            })
+            .on("click", click);
+
+        // 增加文本   节点文字显示左侧还是右侧
+        nodeEnter.append("text")
+            .attr("x", (d) => {
+                // return d.children || d._children ? -10 : 10;
+                // return rectW / 2;
+                if (d.parent) {
+                    d.textPadding = 210 - rectW / 2;
+                    return d.textPadding;
+                } else {
+                    d.textPadding = 10 + rectW / 2;
+                    return d.textPadding;
+                }
+            })
+            .attr("y", (d) => {
+                return 10;
+            })
+            .attr("dy", ".55em")
+            .attr("text-anchor", (d) => {
+                // return d.children || d._children ? "end" : "start";
+                return "start";
+            })
+            .text((d) => {
+                return d.name;
+            })
+            .attr("font-size", (d) => {
+                return "10px";
+            })
+            .call(wrap, 200);
+
+        // Transition nodes to their new position.  // 增加动画延时
+        var nodeUpdate = node.transition()
+            .duration(duration)
+            .attr("transform", (d) => {
+                return "translate(" + d.y + "," + d.x + ")";
+            });
+
+        nodeUpdate.select("circle")
+            .attr("r", 6)
+            .style("fill", (d) => {
+                return d._children ? "lightsteelblue" : "#fff";
+            });
+        // nodeUpdate.select('rect')
+        //     .style("fill", (d) => {
+        //         return d._children ? "lightsteelblue" : "#fff";
+        //     });
+
+        nodeUpdate.select("text")
+            .style("fill-opacity", 1);
+
+        // Transition exiting nodes to the parent's new position.
+        var nodeExit = node.exit().transition()
+            .duration(duration)
+            .attr("transform", (d) => {
+                return "translate(" + source.y + "," + source.x + ")";
+            })
+            .remove();
+
+        nodeExit.select("circle")
+            .attr("r", 1e-6);
+
+        nodeExit.select("text")
+            .style("fill-opacity", 1e-6);
+
+        // Update the links…
+        var link = svg.selectAll("path.link")
+            .data<any>(links, (d) => {
+                return d.target.id;
+            });
+
+        // Enter any new links at the parent's previous position.
+        link.enter().insert("path", "g")
+            .attr("class", "link")
+            .attr("d", (d) => {
+                var o = { x: source.x0, y: source.y0 };
+                return diagonal({ source: o, target: o });
+            });
+
+        // Transition links to their new position.
+        link.transition()
+            .duration(duration)
+            .attr("d", diagonal);
+
+        // Transition exiting nodes to the parent's new position.
+        link.exit().transition()
+            .duration(duration)
+            .attr("d", (d) => {
+                var o = { x: source.x, y: source.y };
+                return diagonal({ source: o, target: o });
+            })
+            .remove();
+
+        // Stash the old positions for transition.
+        nodes.forEach((d: any) => {
+            d.x0 = d.x;
+            d.y0 = d.y;
+        });
+    }
+
+    // Toggle children on click.
+    var click = (d) => {
         if (d.children) {
             d._children = d.children;
-            d._children.forEach(collapse);
             d.children = null;
+        } else {
+            d.children = d._children;
+            d._children = null;
         }
+        update(d);
     }
-
-    data.children.forEach(collapse);
-    update(root);
-});
-
-function wrap(text, width) {
-    text.each(function () {
-        var text = d3.select(this),
-            words = text.text().split(/\s+/).reverse(),
-            word,
-            line = [],
-            lineNumber = 0,
-            lineHeight = 1.1, // ems
-            y = text.attr("y"),
-            dy = parseFloat(text.attr("dy")),
-            tspan = text.text(null).append("tspan").attr("y", y).attr("dy", dy + "em");
-        while (word = words.pop()) {
-            line.push(word);
-            tspan.text(line.join(" "));
-            if ((<SVGTextContentElement>tspan.node()).getComputedTextLength() > width) {
-                line.pop();
-                tspan.text(line.join(" "));
-                line = [word];
-                tspan = text.append("tspan").attr("x", (d) => {
-                    return d.textPadding;
-                }).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
-            }
-        }
-    });
-}
-
-var update = (source) => {
-
-
-    // Compute the new tree layout. 计算父布局并返回一组节点 / 计算树节点的父-子连接。
-    var nodes = tree.nodes(root).reverse(),
-        links = tree.links(nodes);
-
-    // Normalize for fixed-depth.  改变各个层级的距离
-    nodes.forEach((d) => {
-        //  d.y = d.depth * 180;
-        d.y = d.depth * 240;     // translate(180)  0 180 360
-    });
-
-    // Update the nodes…
-    var node = svg.selectAll("g.node")
-        .data<any>(nodes, (d) => {
-            return d.id || (d.id = ++i);
-        });
-
-    // Enter any new nodes at the parent's previous position.  操作之后移动横纵坐标的位置 绑定click事件
-    var nodeEnter = node.enter().append("g")
-        .attr("class", "node")
-        .attr("transform", (d) => {
-            return "translate(" + source.y0 + "," + source.x0 + ")";
-        });
-
-    nodeEnter.append('rect')
-        .attr("width", rectW)
-        .attr("height", rectH)
-        .attr("stroke", "#2ab3ed")
-        .attr("stroke-width", 1)
-        .attr("x", (d) => {
-            return d.parent ? 200 : 0;
-        })
-        .style("fill", "#e8eef7")
-        .attr("clip-path", function (d, i) { return "url(#clip1)"; });
-
-    nodeEnter.append('rect')
-        .attr("width", rectW - 1)
-        .attr("height", 20)
-        .attr("y", (d) => {
-            return 39.5;
-        })
-        .attr("x", (d) => {
-            return d.parent ? 200.5 : 0.5;
-        })
-        .style("fill", "#c8dbf7");
-
-    nodeEnter.append("text")
-        .attr("x", (d) => {
-            return d.parent ? 210 : 10;
-        })
-        .attr("y", (d) => {
-            return rectH - 5;
-        })
-        .attr("text-anchor", (d) => {
-            // return d.children || d._children ? "end" : "start";
-            return "start";
-        })
-        .text((d) => {
-            return "页面访问量:34333112";
-        })
-        .style("fill-opacity", 1e-6);
-
-    //添加节点 如果有字节点颜色加深
-    nodeEnter.append("circle")
-        .attr("cx", (datum, index, outerIndex) => {
-            return rectW;
-        })
-        .attr("cy", (datum, index, outerIndex) => {
-            return rectH / 2;
-        })
-        .attr("stroke", "black")
-        .attr("stroke-width", 1)
-        .attr("r", 6)
-        .style("fill", function (d) {
-            return d._children ? "lightsteelblue" : "#fff";
-        })
-        .on("click", click);
-
-    // 增加文本   节点文字显示左侧还是右侧
-    nodeEnter.append("text")
-        .attr("x", (d) => {
-            // return d.children || d._children ? -10 : 10;
-            // return rectW / 2;
-            if (d.parent) {
-                d.textPadding = 210;
-                return 210;
-            } else {
-                d.textPadding = 10;
-                return 10;
-            }
-        })
-        .attr("y", (d) => {
-            return 10;
-        })
-        .attr("dy", ".55em")
-        .attr("text-anchor", (d) => {
-            // return d.children || d._children ? "end" : "start";
-            return "start";
-        })
-        .text((d) => {
-            return d.name;
-        })
-        .attr("font-size", (d) => {
-            return "10px";
-        })
-        .call(wrap, 200);
-
-    // Transition nodes to their new position.  // 增加动画延时
-    var nodeUpdate = node.transition()
-        .duration(duration)
-        .attr("transform", (d) => {
-            return "translate(" + d.y + "," + d.x + ")";
-        });
-
-    nodeUpdate.select("circle")
-        .attr("r", 6)
-        .style("fill", (d) => {
-            return d._children ? "lightsteelblue" : "#fff";
-        });
-    // nodeUpdate.select('rect')
-    //     .style("fill", (d) => {
-    //         return d._children ? "lightsteelblue" : "#fff";
-    //     });
-
-    nodeUpdate.select("text")
-        .style("fill-opacity", 1);
-
-    // Transition exiting nodes to the parent's new position.
-    var nodeExit = node.exit().transition()
-        .duration(duration)
-        .attr("transform", (d) => {
-            return "translate(" + source.y + "," + source.x + ")";
-        })
-        .remove();
-
-    nodeExit.select("circle")
-        .attr("r", 1e-6);
-
-    nodeExit.select("text")
-        .style("fill-opacity", 1e-6);
-
-    // Update the links…
-    var link = svg.selectAll("path.link")
-        .data<any>(links, (d) => {
-            return d.target.id;
-        });
-
-    // Enter any new links at the parent's previous position.
-    link.enter().insert("path", "g")
-        .attr("class", "link")
-        .attr("d", (d) => {
-            var o = { x: source.x0, y: source.y0 };
-            return diagonal({ source: o, target: o });
-        });
-
-    // Transition links to their new position.
-    link.transition()
-        .duration(duration)
-        .attr("d", diagonal);
-
-    // Transition exiting nodes to the parent's new position.
-    link.exit().transition()
-        .duration(duration)
-        .attr("d", (d) => {
-            var o = { x: source.x, y: source.y };
-            return diagonal({ source: o, target: o });
-        })
-        .remove();
-
-    // Stash the old positions for transition.
-    nodes.forEach((d: any) => {
-        d.x0 = d.x;
-        d.y0 = d.y;
-    });
-}
-
-// Toggle children on click.
-var click = (d) => {
-    if (d.children) {
-        d._children = d.children;
-        d.children = null;
-    } else {
-        d.children = d._children;
-        d._children = null;
-    }
-    update(d);
 }
