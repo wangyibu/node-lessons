d99757ac64d5fb3bd27d8d1ec4fb34b4d471addc
wangyibu <wangyibu@gridsum.com>
2016-08-25 21:44:51 +0800

    11

---
+++ d3_project/tree_layout/svgDemo/script.js
@@ -0,0 +1,10 @@
+var svgDemo;
+(function (svgDemo) {
+    var test;
+    (function (test) {
+        var pre = { id: "pre", cx: "10", cy: "150", r: "5", className: "pre",
+            fill: "none", stroke: "#ddd", strokeWidth: "2" };
+        var next = { id: "next", cx: "390", cy: "150", r: "5", className: "next",
+            fill: "none", stroke: "#ddd", strokeWidth: "2" };
+    })(test = svgDemo.test || (svgDemo.test = {}));
+})(svgDemo || (svgDemo = {}));
